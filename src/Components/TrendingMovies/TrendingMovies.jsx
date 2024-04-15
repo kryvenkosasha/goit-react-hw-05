44b1defe72106aec8694673d42222b2916909914
Olena Kryvenko <bona240609@gmail.com>
2024-04-15 09:06:33 +0200

    update

--- src/Components/TrendingMovies/TrendingMovies.jsx
+++ src/Components/TrendingMovies/TrendingMovies.jsx
@@ -6,18 +6,18 @@ const TrendingMovies = (movies) => {
       <ul>
         {movies.movies.map((movie) => (
           <li key={movie.id}>
-            {/* <NavLink to={`/movies/${movie.id}`}> */}
+            <NavLink to={`/movies/${movie.id}`}>
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
                 alt={movie.title}
               />
               <p>{movie.title}</p>
               <p>Rating: {movie.vote_average}</p>
-            {/* </NavLink> */}
+            </NavLink> 
 
-            {/* <Routes>
+             <Routes>
               <Route path="/movies/${movie.id}" element={<div>dfpm;vc</div>} />
-            </Routes> */}
+            </Routes>
           </li>
         ))}
       </ul>
