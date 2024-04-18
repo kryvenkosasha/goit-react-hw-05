e798efb63fa3a9c5b80f13297dc9fa3874d24a73
Olena Kryvenko <bona240609@gmail.com>
2024-04-18 13:38:08 +0200

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,22 @@
-import { NavLink } from "react-router-dom";
-import './MovieList.css'
+import { NavLink, useLocation } from "react-router-dom";
+import "./MovieList.css";
 
 const TrendingMovies = (movies) => {
+  const location = useLocation();
+
   return (
     <div>
       <ul className="trending-movies-list">
         {movies.movies.map((movie) => (
           <li key={movie.id} className="trending-movies-item">
-            <NavLink to={`/movies/${movie.id}`}>
+            <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
                 alt={movie.title}
               />
               <p>{movie.title}</p>
               <p>Rating: {movie.vote_average}</p>
-            </NavLink> 
-
-             
+            </NavLink>
           </li>
         ))}
       </ul>
